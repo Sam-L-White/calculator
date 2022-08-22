4a4d4f946c80e9977761a72acba7274edbd28907
Sam White <samuelwhite98@outlook.com>
2022-08-22 11:49:17 +0100

    Add calculator logic, add basic CSS

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ function operate(numOne, numTwo, operator){
     switch(operator){
 
         case("+"):
+            numOne = parseFloat(numOne)
+            numTwo = parseFloat(numTwo)
             return add(numOne, numTwo)
         break;
 
         case("-"):
-            return divide(numOne, numTwo)
+            return subtract(numOne, numTwo)
         break;
 
         case("*"):
@@ -35,7 +37,60 @@ function divide(numOne, numTwo){
     return (numOne / numTwo)
 }
 
+let operator
 const buttons = document.querySelectorAll(".button")
 buttons.forEach(button => button.addEventListener('click', function(e){
-    console.log(e)
+
+    const display = document.querySelector(".display");
+    const button = e.target.textContent;
+
+   
+    
+    
+
+    switch(button){
+
+        case("+"):
+            operator = "+"
+            numOne = display.textContent
+            display.textContent = ""
+        break;
+
+        case("-"):
+            operator = "-"
+            numOne = display.textContent
+            display.textContent = ""
+        break;
+
+        case("x"):
+            operator = "*"
+            numOne = display.textContent
+            display.textContent = ""
+        break;
+
+        case("/"):
+            operator = "/"
+            numOne = display.textContent
+            display.textContent = ""
+        break;
+
+        case("="):
+            numTwo = display.textContent
+            result = operate(numOne, numTwo, operator)
+            display.textContent = result
+            
+        break;
+
+        default:
+            
+                display.textContent = display.textContent + button
+            
+            
+
+            
+    }
+
+   
+
+    
 }))
